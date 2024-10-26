eac9855c0608bb8d8b8f2e491bf28f7fa0f8e1ff
yqshe <yqshe@aibee.com>
2024-10-26 16:32:45 +0800

    close download in grounding_dino.py

--- masa/models/detectors/grounding_dino.py
+++ masa/models/detectors/grounding_dino.py
@@ -23,8 +23,8 @@ try:
     import nltk
 
     download_dir = os.path.expanduser("~/nltk_data")
-    nltk.download("punkt", download_dir=download_dir, quiet=True)
-    nltk.download("averaged_perceptron_tagger", download_dir=download_dir, quiet=True)
+    # nltk.download("punkt", download_dir=download_dir, quiet=True)
+    # nltk.download("averaged_perceptron_tagger", download_dir=download_dir, quiet=True)
 except ImportError:
     raise RuntimeError(
         "nltk is not installed, please install it by: " "pip install nltk."
