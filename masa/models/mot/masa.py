1ae48650889c38e87eb67ae29604e4d7011013ce
yqshe <yqshe@aibee.com>
2024-12-30 15:46:51 +0800

    control using aibee_model or official model by config

--- masa/models/mot/masa.py
+++ masa/models/mot/masa.py
@@ -92,7 +92,7 @@ class MASA(BaseMOTModel):
 
         if detector is not None:
             self.detector = MODELS.build(detector)
-
+        self.detector_type = detector['type']
         if masa_adapter is not None:
             self.masa_adapter = MODELS.build(masa_adapter)
 
@@ -447,8 +447,9 @@ class MASA(BaseMOTModel):
             else:
                 x = self.detector.backbone.forward(key_imgs)
                 ref_x = self.detector.backbone.forward(ref_imgs)
-        x = x[1:]
-        ref_x = ref_x[1:]
+        if self.detector_type == "RetinaNet":
+            x = x[1:]
+            ref_x = ref_x[1:]
         x_m = self.masa_adapter(x)
         ref_x_m = self.masa_adapter(ref_x)
 
